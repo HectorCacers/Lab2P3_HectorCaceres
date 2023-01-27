48e2b68bf6a186cdfdd3449923361be29a39e461
hector <hectorcacers11@gmail.com>
2023-01-27 15:41:42 -0600

    second

--- Lab2Prog3_HectorCaceres.cpp
+++ Lab2Prog3_HectorCaceres.cpp
@@ -10,6 +10,22 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	int op = 0;
+	//////////////////////////////////////
+	double sueldo = 0, suma = 0, promedio = 0;
+	int falla = 0, fa = 0;
+	int j = 6;
+	double suma1 = 0, promedio1 = 0;
+	double bono = 0.05, rr = 0, total = 0;
+	/////////////////////////////////////
+	int ymax = 0, xmax = 0;
+	int b = 0;
+	double m = 0;
+	//////////////////////////////////////
+	int dia1=0, mes1=0, ano1=0;
+	int dia2=0, mes2=0, ano2=0;
+	int diferencia = 0, dias = 0;
+	int r = 0;
+	//////////////////////////////////////
 
 	cout << "\n\n" << endl;
 
@@ -33,6 +49,84 @@ int main(int argc, char** argv)
 		switch (op) {
 
 		case 1:
+			suma = 0;
+			suma1 = 0;
+			fa = 0;
+
+
+
+			cout << "EJERCICIO (1)" << endl;
+			cout << "-----------------" << endl;
+			cout << "\n" << endl;
+
+			for (int i = 0; i < 12; i++) {
+
+				cout << "\n\n" << endl;
+				cout << "MES Nº (" << (i + 1) << ")" << endl;
+				cout << "\n\n" << endl;
+
+				cout << "SUELDO_MES : ";
+				cin >> sueldo;
+				cout << "DIAS_FALTADOS_MES : ";
+				cin >> falla;
+
+				suma = suma + sueldo;
+
+				cout << "\n\n" << endl;
+
+				if ((i + 1) > j) {
+					suma1 = suma1 + sueldo;
+
+					if (falla > 2) {
+						fa = fa + 1;
+					}
+
+				}
+
+
+			}
+
+			cout << "SUMA : " << suma;
+			promedio = (suma / 12);
+			cout << "PROMEDIO TOTAL : " << promedio << endl;
+			cout << "\n\n" << endl;
+
+			cout << "EL AGINALDO DEL EMPLEADO ES $" << promedio << endl;
+
+			cout << "\n\n" << endl;
+
+			cout << "  BONIFICACION" << endl;
+			cout << "-----------------" << endl;
+			cout << "\n" << endl;
+
+			if (fa > 2) {
+				cout << "! NO HAY BONIFICACION !" << endl;
+			}
+			else {
+				cout << "! SI HAY BONIFICACION ! --> 5%" << endl;
+				cout << "\n\n" << endl;
+
+				cout << "SUMA : " << suma1;
+				promedio1 = (suma1 / j);
+				cout << "PROMEDIO TOTAL : " << promedio1 << endl;
+				cout << "\n\n" << endl;
+
+				cout << "5% DE BONIFICACION $ ?" << endl;
+				cout << "PROMEDIO TOTAL $" << promedio1 << endl;
+				rr = (promedio1 * bono);
+				cout << "5% DEL PROMEDIO $" << rr << endl;
+				total = promedio + rr;
+				cout << "TOTAL DEL BONO + EL PROMEDIO TOTAL $" << total << endl;
+
+				cout << "\n\n" << endl;
+
+				cout << "--------------------------------------" << endl;
+				cout << "TOTAL A RECIBIR POR TODO ES $" << total << endl;
+				cout << "--------------------------------------" << endl;
+
+
+			}
+
 			break;
 		case 2:
 			break;
